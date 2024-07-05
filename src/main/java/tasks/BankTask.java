f9d81cd93639a4e8c18e993897336b9b1c66adac
Manokit <bbargay@gmail.com>
2024-07-04 18:54:39 -0700

    test

--- src/main/java/tasks/BankTask.java
+++ src/main/java/tasks/BankTask.java
@@ -1,6 +1,9 @@
 package tasks;
 
 import helpers.*;
+import helpers.utils.*;
+import utils.Task;
+import static main.PlankMaker.*;
 import helpers.annotations.AllowedValue;
 import helpers.annotations.ScriptConfiguration;
 import helpers.annotations.ScriptManifest;
@@ -10,10 +13,6 @@ import tasks.*;
 import helpers.AbstractScript;
 import helpers.ScriptCategory;
 
-import helpers.utils.*;
-import utils.Task;
-
-import static main.PlankMaker.*;
 import static helpers.Interfaces.*;
 
 import java.awt.Color;
@@ -24,19 +23,18 @@ import java.util.Map;
 import java.util.Random;
 
 public class BankTask extends Task {
-    private static final Tile BANK_TILE = new Tile(587, 852);
     private String dynamicBank = null;
 
     @Override
     public boolean activate() {
-        return Inventory.isFull() || (!Inventory.contains(logItemId, 0.80) && !Inventory.contains(ItemList.COINS_995, 0.80));
+        return Inventory.isFull() || (!Inventory.contains(logItemId, 0.80) || !Inventory.contains(ItemList.COINS_995, 0.80));
     }
 
     @Override
     public boolean execute() {
-        if (Walker.getPlayerPosition() != BANK_TILE) {
+        if (!Player.isTileWithinArea(Walker.getPlayerPosition(), AT_BANK_AREA)) {
             Logger.log("Not at the bank, walking there");
-            Walker.walkTo(BANK_TILE);
+            Walker.walkPath(PATH_TO_BANK);
             return false;
         }
         handleBanking();
@@ -78,11 +76,9 @@ public class BankTask extends Task {
             Condition.wait(Bank::isSelectedQuantityAllButton, 100, 20);
         }
         
-        // Deposit logs if inventory is full
-        if (Inventory.isFull()) {
-            Inventory.tapItem(logItemId, 0.60);
-            Condition.wait(() -> !Inventory.isFull(), 100, 10);
-        }
+        // Deposit all items
+        Bank.tapDepositInventoryButton();
+        //Condition.wait(Inventory::isEmpty, 100, 10);
         
         // Withdraw coins if not in inventory
         if (!Inventory.contains(ItemList.COINS_995, 0.80)) {
