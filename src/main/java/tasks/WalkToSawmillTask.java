726e4bb42014bc72cd01bc967f14bd4c03aebae5
Manokit <bbargay@gmail.com>
2024-07-04 18:24:37 -0700

    walk to sawmill looking ok

---
+++ src/main/java/tasks/WalkToSawmillTask.java
@@ -0,0 +1,21 @@
+package tasks;
+
+import helpers.utils.Tile;
+import utils.Task;
+import static main.PlankMaker.*;
+import static helpers.Interfaces.*;
+
+public class WalkToSawmillTask extends Task {
+    @Override
+    public boolean activate() {
+        Tile playerPosition = Walker.getPlayerPosition();
+        return Inventory.contains(logItemId, 0.80) && !Player.isTileWithinArea(playerPosition, AT_SAWMILL_AREA);
+    }
+
+    @Override
+    public boolean execute() {
+        Logger.log("Walking to sawmill");
+        Walker.walkPath(WOODCUTTING_GUILD_REGION, PATH_TO_SAWMILL);
+        return false;
+    }
+}
